6a886b355c0562565ba715eae8d480f606b8a77e
coderJakub <kliemann.jakub@gmail.com>
2023-10-24 00:09:17 +0200

    cleaned code

--- Lexer.java
+++ Lexer.java
@@ -9,7 +9,99 @@ public class Lexer{
     Token t;
     int state;
 
-    public static class Token{
+    //Definieren der Zeichenklassen für kleinere Automatentabelle
+    // 0:Sonderzeichen, 1: Ziffer, 2:Buchstabe 3: : 4: = 5: < 6: > 7: sonstige Steuerzeichen 
+    char[] signClass={
+        /*     0  1  2  3  4  5  6  7  8  9  A  B  C  D  E  F */
+        /*---------------------------------------------------------*/
+        /* 0*/ 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, /* 0*/
+        /*10*/ 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, /*10*/
+        /*20*/ 7, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, /*20*/
+        /*30*/ 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 3, 0, 5, 4, 6, 0, /*30*/
+        /*40*/ 0, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, /*40*/
+        /*50*/ 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 0, 0, 0, 0, 0, 0, /*50*/
+        /*60*/ 0, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, /*60*/
+        /*70*/ 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 0, 0, 0, 0, 0  /*70*/
+    };
+
+    //Automatentabelle (Zustand 9: Endzustand)           für Steuerzeichen SLB??? wenn Puffer nicht leer, dann B, sonst SLB
+    state[][] automat={
+        /*z    So,                  Zif,                Bu,                    :,                   =,                    <,                   >,                   Steur   */
+        /*------------------------------------------------------------------------------------------------------------------------------------------------------------------*/
+        /*0*/{new stateSLB(9),new stateSL(1),new stateSGL(2),new stateSL(3),new stateSLB(9),new stateSL(4),new stateSL(5),new stateSLB(9)},
+        /*1*/{  new stateB(9),new stateSL(1),  new stateB(9), new stateB(9),  new stateB(9), new stateB(9), new stateB(9),new stateB(9)},
+        /*2*/{  new stateB(9),new stateSL(2),new stateSGL(2), new stateB(9),  new stateB(9), new stateB(9), new stateB(9),new stateB(9)},
+        /*3*/{  new stateB(9), new stateB(9),  new stateB(9), new stateB(9), new stateSL(6), new stateB(9), new stateB(9),new stateB(9)},
+        /*4*/{  new stateB(9), new stateB(9),  new stateB(9), new stateB(9), new stateSL(7), new stateB(9), new stateB(9),new stateB(9)},
+        /*5*/{  new stateB(9), new stateB(9),  new stateB(9), new stateB(9), new stateSL(8), new stateB(9), new stateB(9),new stateB(9)},
+        /*6*/{  new stateB(9), new stateB(9),  new stateB(9), new stateB(9),  new stateB(9), new stateB(9), new stateB(9),new stateB(9)},
+        /*7*/{  new stateB(9), new stateB(9),  new stateB(9), new stateB(9),  new stateB(9), new stateB(9), new stateB(9),new stateB(9)},
+        /*8*/{  new stateB(9), new stateB(9),  new stateB(9), new stateB(9),  new stateB(9), new stateB(9), new stateB(9),new stateB(9)}
+    };
+
+    //Hauptzustandsklasse
+    abstract public class state{
+        int nextS;
+        public state(int state){
+            nextS=state;
+        }
+        abstract void func();
+    }
+
+    //Unterzustandsklassen
+    //Zustand mit Schreib- und lesefunktion
+    public class stateSL extends state{
+        public stateSL(int state){
+            super(state);
+        }
+        void func(){
+            sl();
+        }
+    }
+
+    //Zustand mit Schreib- und lesefunktion (Kleinbuchstaben werden zu Großbuchstaben)
+    public class stateSGL extends state{
+        public stateSGL(int state){
+            super(state);
+        }
+        void func(){
+            x-=(x>65&&x<90)?0:(97-65);
+            sl();
+        }
+    }
+
+    //Zustand mit Schreibfunktion
+    public class stateL extends state{
+        public stateL(int state){
+            super(state);
+        }
+        void func(){
+            l();
+        }
+    }
+
+    //Zustand mit Beeendenfunktion
+    public class stateB extends state{
+        public stateB(int state){
+            super(state);
+        }
+        void func(){
+            b();
+        }
+    }
+
+    //Zustand mit Schreib-, Lese- und Beendenfunktion
+    public class stateSLB extends state{
+        public stateSLB(int state){
+            super(state);
+        }
+        void func(){
+            sl(); b();
+        }
+    }
+
+    //Tokenklasse
+    public static class Token{ //static, da sonst nicht von Test.java aus zugreifbar -> Lösung? 
         int len;
         int type; //0->Empty, 1->Sym, 2->Num, 3->Ident
         int posLine;
@@ -29,7 +121,7 @@ public class Lexer{
             sym=0;
         }
     }
-    //Nicht möglich da TokenTyp erst später bekannt wird
+    //Schlecht zu implementieren, da Tokenklasse bekannt sein muss um Datentyp des Inhalts zu bestimmen -> obere Variante besser?
     /*public class NumToken extends Token{
         long num;
         int getType(){
@@ -48,6 +140,8 @@ public class Lexer{
             return 2;
         }
     }*/
+
+    //Konstruktoren
     public Lexer(){};
     public Lexer(String fileName){
         if(!fileName.contains(".pl0"))fileName+=".pl0";
@@ -68,10 +162,11 @@ public class Lexer{
         }
     }
 
+    //Lexfunktion
     Token Lex(){
         state zx;
         t=new Token();
-        buf="";
+        buf=""; //Aktuell nicht ein Buchstabe im Vorraus bekannt -> Puffer nicht komplett löschen?
         state=0;
         do{
             zx=automat[state][signClass[x]];
@@ -82,6 +177,7 @@ public class Lexer{
         return t;
     }
 
+    //Hilfsfunktionen für Zustandsklassen
     void l(){
         try{
             x = (char)fis.read();   
@@ -128,82 +224,4 @@ public class Lexer{
                 break;
         }
     }
-
-    abstract public class state{
-        int nextS;
-        public state(int state){
-            nextS=state;
-        }
-        abstract void func();
-    }
-    public class stateSL extends state{
-        public stateSL(int state){
-            super(state);
-        }
-        void func(){
-            sl();
-        }
-    }
-    public class stateSGL extends state{
-        public stateSGL(int state){
-            super(state);
-        }
-        void func(){
-            x-=(x>65&&x<90)?0:(97-65);
-            sl();
-        }
-    }
-    public class stateL extends state{
-        public stateL(int state){
-            super(state);
-        }
-        void func(){
-            l();
-        }
-    }
-    public class stateB extends state{
-        public stateB(int state){
-            super(state);
-        }
-        void func(){
-            b();
-        }
-    }
-    public class stateSLB extends state{
-        public stateSLB(int state){
-            super(state);
-        }
-        void func(){
-            sl(); b();
-        }
-    }
-    // 0:Sonderzeichen, 1: Ziffer, 2:Buchstabe 3: : 4: = 5: < 6: > 7: sonstige Steuerzeichen 
-    char[] signClass={
-        /*     0  1  2  3  4  5  6  7  8  9  A  B  C  D  E  F */
-        /*---------------------------------------------------------*/
-        /* 0*/ 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, /* 0*/
-        /*10*/ 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, /*10*/
-        /*20*/ 7, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, /*20*/
-        /*30*/ 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 3, 0, 5, 4, 6, 0, /*30*/
-        /*40*/ 0, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, /*40*/
-        /*50*/ 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 0, 0, 0, 0, 0, 0, /*50*/
-        /*60*/ 0, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, /*60*/
-        /*70*/ 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 0, 0, 0, 0, 0  /*70*/
-    };
-
-    //func: 0: keine, 1: sl, 2:sgl, 3:l, 4:b, 5:slb
-    state[][] automat={
-        /*z    So,                  Zif,                Bu,                    :,                   =,                    <,                   >,                   Steur   */
-        /*------------------------------------------------------------------------------------------------------------------------------------------------------------------*/
-        /*0*/{new stateSLB(9),new stateSL(1),new stateSGL(2),new stateSL(3),new stateSLB(9),new stateSL(4),new stateSL(5),new stateSLB(9)},
-        /*1*/{  new stateB(9),new stateSL(1),  new stateB(9), new stateB(9),  new stateB(9), new stateB(9), new stateB(9),new stateB(9)},
-        /*2*/{  new stateB(9),new stateSL(2),new stateSGL(2), new stateB(9),  new stateB(9), new stateB(9), new stateB(9),new stateB(9)},
-        /*3*/{  new stateB(9), new stateB(9),  new stateB(9), new stateB(9), new stateSL(6), new stateB(9), new stateB(9),new stateB(9)},
-        /*4*/{  new stateB(9), new stateB(9),  new stateB(9), new stateB(9), new stateSL(7), new stateB(9), new stateB(9),new stateB(9)},
-        /*5*/{  new stateB(9), new stateB(9),  new stateB(9), new stateB(9), new stateSL(8), new stateB(9), new stateB(9),new stateB(9)},
-        /*6*/{  new stateB(9), new stateB(9),  new stateB(9), new stateB(9),  new stateB(9), new stateB(9), new stateB(9),new stateB(9)},
-        /*7*/{  new stateB(9), new stateB(9),  new stateB(9), new stateB(9),  new stateB(9), new stateB(9), new stateB(9),new stateB(9)},
-        /*8*/{  new stateB(9), new stateB(9),  new stateB(9), new stateB(9),  new stateB(9), new stateB(9), new stateB(9),new stateB(9)}
-    };
-
 }

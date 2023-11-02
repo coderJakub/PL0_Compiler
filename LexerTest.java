585fd056fd8b9dceb85a6a256e0fd38eccfd9893
coderJakub <kliemann.jakub@gmail.com>
2023-11-02 11:03:27 +0100

    Lexer Test upgrade

--- LexerTest.java
+++ LexerTest.java
@@ -1,16 +1,20 @@
 
-public class Test extends Lexer{
+public class LexerTest extends Lexer{
     
     public static void main(String args[]){
         if(args.length!=1)return;
         Lexer lexer = new Lexer(args[0]);
         Lexer.Token t = lexer.new Token(); //sodass auf eine none static inner class zugegriffen werden kann
+        
+        String[] sym= {":=", "<=", ">=", "BEGIN", "CALL", "CONST", "DO", "END", "IF", "ODD", "PROCEDURE", "THEN", "VAR", "WHILE"};
+        
         while(t.type!=1 || t.sym!='.'){
             t=lexer.Lex();
             switch(t.type){
                 case 1:
                     if(t.sym<128)System.out.println("Symbol: "+(char)t.sym);
-                    else System.out.println("Symbol: "+t.sym);
+                    else 
+                        System.out.println("Symbol: "+sym[t.sym-128]);
                     break;
                 case 2:
                     System.out.println("Number: "+t.num);
